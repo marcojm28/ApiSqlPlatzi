f33355c0093137cd7ed0ddef92de11034f98bb21
Marco Juárez <mjuarezm@canvia.com>
2020-04-21 19:52:02 -0500

    Se añade clase controller

---
+++ PlatzisqlAzure/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PlatzisqlAzure.Models;
+
+namespace PlatzisqlAzure.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly ContactContext contactContext;
+
+        public ContactController(ContactContext context)
+        {
+            contactContext = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return contactContext.GetContacts.ToList();
+        }
+
+        ~ContactController()
+        {
+            contactContext.Dispose();
+        }
+
+    }
+}
